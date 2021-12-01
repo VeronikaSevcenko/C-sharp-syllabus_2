866772457fe61520e44209d16d52fb05fa25de03
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-01 16:05:28 +0300

    Exercise 2

---
+++ csharp-basics/exercises/Arithmetic/CheckOddEven/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+                Console.WriteLine("Bye");
+            }
+            else
+            {
+                Console.WriteLine("Odd Number");
+                Console.WriteLine("Bye");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
