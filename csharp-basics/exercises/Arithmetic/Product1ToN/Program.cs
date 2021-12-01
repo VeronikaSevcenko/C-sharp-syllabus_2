32f9e705cfea3070f2e2ef9a495871ca2e0fd9d6
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-01 16:28:36 +0300

    Exercise 4

---
+++ csharp-basics/exercises/Arithmetic/Product1ToN/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product1ToN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                sum *= i;
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
