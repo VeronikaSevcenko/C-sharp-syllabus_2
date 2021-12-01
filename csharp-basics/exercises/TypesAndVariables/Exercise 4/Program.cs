b98f2a60194cef88f4439019c2c925f0a5c59ab2
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-01 20:16:08 +0300

    Exercise 4

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Veronika Ševčenko";
+            int myAge = 1995;
+
+            Console.WriteLine("My name is " 
+                + myName + " " 
+                + "and I was born in " 
+                + myAge + ".");
+
+            Console.ReadLine();
+        }
+    }
+}
