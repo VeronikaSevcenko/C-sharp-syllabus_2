a3ee2c9512dfb25afd756013bf602f7c249bc7bc
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-01 23:08:51 +0300

    Exercise 3 done

--- csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
@@ -1,24 +1,24 @@
-﻿namespace Exercise3
+﻿using System;
+
+namespace Exercise3
 {
     class Program
     {
         //TODO: Write a C# program to calculate the average value of array elements.
         private static void Main(string[] args)
         {
-            int[] numbers = {20, 30, 25, 35, -16, 60, -100};
-
-            /*
-            fixme - calculate sum of all array elements 
+            int[] numbers = { 20, 30, 25, 35, -16, 60, -100 };
             int sum = 0;
-            for (?){
-                
-            }
-            */
+            float average;
 
-            /*
-            fixme - calculate average value 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            average = sum / numbers.Length;
+
+            Console.WriteLine("The sum is " + sum);
             Console.WriteLine("Average value of the array elements is : " + average);
-            */
         }
     }
 }
