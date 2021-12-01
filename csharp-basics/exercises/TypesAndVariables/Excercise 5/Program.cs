34ceed2e03fd20e1631baa8c553d8a7d94249432
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-01 20:18:40 +0300

    Exercise 5

---
+++ csharp-basics/exercises/TypesAndVariables/Excercise 5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Excercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string classes1 = "English III";
+            string classes2 = "Precalculus";
+            string classes3 = "Music Theory";
+            string classes4 = "Biotechnology";
+            string classes5 = "Latin II";
+
+            string teacher1 = "Ms. Lapan";
+            string teacher2 = "Mrs. Gideon";
+            string teacher3 = "Mr. Davis";
+            string teacher4 = "Ms. Pulma";
+            string teacher5 = "Ms. Ziona";
+
+            Console.WriteLine("+-----------------------------------------+");
+            Console.WriteLine("|1|             " + classes1 + " |   " + teacher1 + " |");
+            Console.WriteLine("|2|             " + classes2 + " | " + teacher2 + " |");
+            Console.WriteLine("|3|            " + classes3 + " |  " + teacher3 + "  |");
+            Console.WriteLine("|4|           " + classes4 + " | " + teacher4 + "   |");
+            Console.WriteLine("|5|                " + classes5 + " |    " + teacher5 + "|");
+            Console.WriteLine("+----------------------------------------+");
+        }
+    }
+}
