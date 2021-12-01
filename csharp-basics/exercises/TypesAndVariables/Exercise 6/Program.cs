6193499efa825600f6095f65bfeae2eec4639354
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-01 20:23:07 +0300

    Exercise 6

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int Sum = 0;
+
+            Console.WriteLine("Enter a series of single digit numbers");
+            a = int.Parse(Console.ReadLine());
+
+            while (a > 0)
+            {
+                Sum += a % 10;
+                a /= 10;
+
+                Console.WriteLine("Sum of all the numbers: " + Sum);
+                Console.ReadLine();
+            }
+        }
+    }
+}
