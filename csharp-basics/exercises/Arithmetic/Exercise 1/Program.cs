1425eddbc02f33ce3a21ad56413a3c46a8271ed1
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-01 16:03:23 +0300

    Exercise 1

---
+++ csharp-basics/exercises/Arithmetic/Exercise 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.WriteLine("First integer");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Second integer");
+            b = int.Parse(Console.ReadLine());
+
+            bool result = a == 15 || b == 15 || a + b == 15 || Math.Abs(a - b) == 15;
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
