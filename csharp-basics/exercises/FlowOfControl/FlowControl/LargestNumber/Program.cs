574f3d897353a613f31f923ea9428ba9113de7f8
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-03 23:49:58 +0300

    corrected Exercise 1

--- csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
@@ -18,7 +18,7 @@ namespace LargestNumber
 
             if (input1 > input2)
             {
-                if (input2 > input3)
+                if (input1 > input3)
                 {
                     Console.WriteLine("Number 1st is largest");
                 }
