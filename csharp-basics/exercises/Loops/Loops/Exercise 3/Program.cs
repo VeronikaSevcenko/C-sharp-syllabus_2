2434200adef6916e3a0fba9121d0d657e70bbc4a
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-08 23:40:25 +0300

    Exercise 3 Loops

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[50];
+            Random rnd = new Random();
+            Console.WriteLine("Random numbers: ");
+            Console.WriteLine();
+
+            for (int i = 0; i < 20; i++)
+            {
+                int result = rnd.Next(50);
+                numbers[i] = result;
+                Console.Write(numbers[i] + "  ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Which index  of number you want to know?");
+            
+            int z = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < 20; i++)
+            {
+                if (numbers[i] == z)
+                {
+                    Console.Write(i + ",");
+                }
+            }
+        }
+    }
+}
