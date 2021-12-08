f130eb6a99c58cc81e24616d4be41d699b566581
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-08 23:48:24 +0300

    Exercise 5 Loops

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            string b;
+            int i;
+
+            Console.WriteLine("Enter first word: ");
+            a = Console.ReadLine();
+
+            Console.WriteLine("Enter second word: ");
+            b = Console.ReadLine();
+
+            var result = a + " " + b;
+            var line = result.Length;
+            int period = 0;
+
+            if (line <= 30) period = 30 - line;
+            var dot = "";
+            for (i = 1; i <= period; i++) dot += ".";
+            {
+                Console.WriteLine(a + dot + b);
+            }
+        }
+    }
+}
