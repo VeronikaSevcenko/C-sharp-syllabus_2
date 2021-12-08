db605eab1d788eda8ec8fe4ecb553f018f9ee922
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-09 00:09:30 +0300

    Exercise 9 Loops

---
+++ csharp-basics/exercises/Loops/Loops/RollTwoDice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RollTwoDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sum;
+            Random rnd = new Random();
+            int desired1 = rnd.Next(6) + 1;
+            int desired2 = rnd.Next(6) + 1;
+            int SumOfTheTwoDice = desired1 + desired2;
+            int count = 0;
+
+            Console.WriteLine("What amount do you want?");
+            Sum = int.Parse(Console.ReadLine());
+            Console.WriteLine(desired1 + " and " + desired2 + " = " + SumOfTheTwoDice);
+
+            while (SumOfTheTwoDice != Sum)
+            {
+                desired1 = rnd.Next(6) + 1;
+                desired2 = rnd.Next(6) + 1;
+                SumOfTheTwoDice = desired1 + desired2;
+                count++;
+                Console.WriteLine(desired1 + " and " + desired2 + " = " + SumOfTheTwoDice);
+            }
+        }
+    }
+}
