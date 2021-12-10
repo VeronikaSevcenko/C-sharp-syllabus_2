4b048aeffade5272af825ca6ef60fbc65e0070d5
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-10 20:32:16 +0300

    Exercise 6 Loops

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Write a number");
+            number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("FizzBuzz" + " ");
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Buzz" + " ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Fizz" + " ");
+                }
+                else
+                {
+                    Console.Write(i + ((i+ (1-1)) % 20 == 0 ? "\n" : " "));
+                }
+            }
+        }
+    }
+}
