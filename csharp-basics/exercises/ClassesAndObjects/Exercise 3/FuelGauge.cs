ea74e4baabfbaadf8184be09d9e5a6ea5ee6d8b0
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-12 16:01:26 +0300

    Exercise 3

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 3/FuelGauge.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_3
+{
+    class FuelGauge
+    {
+        public  int MaxLitr = 70;
+
+        private int Litr;
+
+        public FuelGauge()
+        {
+            Litr = 0;
+        }
+
+        public FuelGauge(int Litr)
+        {
+            if (Litr <= MaxLitr)
+            {
+                this.Litr = Litr;
+            }
+            else
+            {
+                Litr = MaxLitr;
+            }
+        }
+        public int GetLitr()
+        {
+            return Litr;
+        }
+
+        public void AddLitr()
+        {
+            if (Litr < MaxLitr)
+            {
+                Litr++;
+            }
+            else
+            {
+                Console.WriteLine("It's Full");
+            }
+        }
+        public void Burn()
+        {
+            if (Litr > 0)
+            {
+                Litr--;
+            }
+            else
+            {
+                Console.WriteLine("No Fuel");
+            }
+        }
+    }
+}
