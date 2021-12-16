071c44755585e8a08070756978ac235160e932ef
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-16 13:48:56 +0300

    Exercise 4

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var SpiderMan = new Movie("Spider-Man", "Columbia Pictures", "PG");
+            var CasinoRoyal = new Movie("Casino Royale", "Eon Productions", "PG13");
+            var Glass = new Movie("Glass", "Buena Vista International", "PG13");
+            var movies = new List<Movie>
+            {
+                SpiderMan,
+                CasinoRoyal,
+                Glass
+            };
+            var filtred = GetPG(movies.ToArray());
+            foreach (var movie in filtred)
+            {
+                Console.WriteLine($"{movie}");
+                Console.WriteLine($"{movie.Display()}");
+            }
+        }
+
+        public static Movie[] GetPG(Movie[] movies)
+        {
+            var filtred = new List<Movie>();
+            foreach (var movie in movies)
+            {
+                if(movie.Rating == "PG")
+                {
+                    filtred.Add(movie);
+                }
+            }
+            return filtred.ToArray();
+        }
+    }
+}
