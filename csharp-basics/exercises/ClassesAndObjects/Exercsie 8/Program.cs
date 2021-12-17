bcd31da77ddb59fd0a9c1bb2cf58c753c2520e84
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-17 16:20:48 +0300

    Exercsie 8

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercsie 8/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Exercsie_8
+{
+    class SavingsAccount
+    {
+        private double balance;
+        private double withdraw;
+        private double deposit;
+        private double interestRate;
+        private double interest;
+        private double totalDeposit;
+        private double totalWithdrawn;
+
+        public SavingsAccount(double startBalance, double inteRate)
+        {
+            balance = startBalance;
+            interestRate = inteRate;
+            interest = 0.00;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void GetDeposit(double depositAmount)
+        {
+            deposit = depositAmount;
+        }
+
+        public void AddDeposit()
+        {
+            balance = (balance + deposit);
+        }
+
+        public void SetWithdraw(double withdrawAmount)
+        {
+            withdraw = withdrawAmount;
+        }
+
+        public void subtractWithdraw()
+        {
+            balance = (balance - withdraw);
+        }
+
+        public double GetInterestRate()
+        {
+            return interestRate;
+        }
+
+        public void CalculateInterest()
+        {
+            interest = (interestRate / 12);
+            interest = (balance * interest);
+        }
+
+        public void AddInterest()
+        {
+            balance = (balance + interest);
+        }
+
+        public double GetInterest()
+        {
+            return interest;
+        }
+
+        public void AddTotalDeposit()
+        {
+            totalDeposit = totalDeposit + deposit;
+        }
+
+        public double GetTotalDeposit()
+        {
+            return totalDeposit;
+        }
+
+        public void AddTotalWithdraw()
+        {
+            totalWithdrawn = totalWithdrawn + withdraw;
+        }
+
+        public double GetTotalWithdraw()
+        {
+            return totalWithdrawn;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercsie 8/Test.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Exercsie_8
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            double balance = 0.00;
+            double interestRate = 0.00;
+            int months = 0;
+            double depositAmount;
+            double withdrawAmount;
+            double totalDeposits = 0.00;
+            double totalWithdrawn = 0.00;
+
+            SavingsAccount Account = new SavingsAccount(balance, interestRate);
+
+            Console.WriteLine("How much money is in the account?:");
+            balance = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the annual interest rate:");
+            interestRate = double.Parse(Console.ReadLine());
+            Account.GetInterestRate();
+
+            Console.WriteLine("How long has the account been opened?(months):");
+            months = int.Parse(Console.ReadLine());
+            for (int month = 1; month <= months; month++)
+            {
+                balance = Account.GetBalance();
+            }
+
+            Console.WriteLine("Enter amount deposited for month:" + months);
+            depositAmount = double.Parse(Console.ReadLine());
+            Account.AddDeposit();
+            totalDeposits = (totalDeposits + depositAmount);
+
+            Console.WriteLine("Enter amount withdrawn for 1:" + months);
+            withdrawAmount = double.Parse(Console.ReadLine());
+            Account.subtractWithdraw();
+            totalWithdrawn = (totalWithdrawn + withdrawAmount);
+            Account.CalculateInterest();
+            Account.AddInterest();
+
+            Account.AddTotalDeposit();
+            Account.GetTotalDeposit();
+            Console.WriteLine($"Total deposited:${totalDeposits}");
+
+            Account.AddTotalWithdraw();
+            Account.GetTotalDeposit();
+            Console.WriteLine($"Total withdrawn:${totalWithdrawn}");
+
+            Console.WriteLine($"Ending balance:${Account.GetBalance()}");
+
+            Account.AddInterest();
+            Account.GetInterest();
+            Console.WriteLine($"Interest earned:${interestRate}");
+        }
+    }
+}
