4b3998f0ebb34b8e239a579a939cb858d987dd9a
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-25 20:25:33 +0300

    4 Exercise

---
+++ csharp-basics/exercises/Collections/Exercise 4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            int number;
+            int sum = 0;
+
+            Console.WriteLine("Enter the number ");
+            number = int.Parse(Console.ReadLine());
+
+            while (sum != 1 && sum != 4)
+            {
+                sum = 0;
+                while (number > 0)
+                {
+                    j = number % 10;
+                    sum += (j * j);
+                    number /= 10;
+                }
+                number = sum;
+            }
+
+            if (sum == 1)
+            {
+                Console.WriteLine("Happy number ");
+            }
+            else
+            {
+                Console.WriteLine("Unhappy number ");
+            }
+        }
+    }
+}
