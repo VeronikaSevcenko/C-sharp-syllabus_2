cfb49698ad9ca96e297827e764db80e845f846cb
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-25 17:27:41 +0300

    5 Exercise

--- csharp-basics/exercises/Collections/WordCount/Program.cs
+++ csharp-basics/exercises/Collections/WordCount/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
 
 namespace WordCount
 {
@@ -10,7 +13,32 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int countLines = File.ReadAllLines(@"C:\Users\37123\Desktop\C-sharp-syllabus_2\csharp-basics\exercises\Collections\WordCount\lear.txt").Length;
+            
+            StreamReader file = new StreamReader(@"C:\Users\37123\Desktop\C-sharp-syllabus_2\csharp-basics\exercises\Collections\WordCount\lear.txt");
+
+            int countWords = 0;
+            int countChars = 0;
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                String[] words = line.Split(' ');
+
+                countWords += words.Length;
+
+                string[] text = File.ReadAllLines(@"C:\Users\37123\Desktop\C-sharp-syllabus_2\csharp-basics\exercises\Collections\WordCount\lear.txt");
+
+                for (int i = 0; i < text.Count(); i++)
+                {
+                    countChars += text[i].Length;
+                }
+            }
+            file.Close();
+        
+            Console.WriteLine("Lines = {0} ", +countLines);
+            Console.WriteLine("Words = {0}", +countWords);
+            Console.WriteLine("Chars = {0}", +countChars);
         }
     }
 }
