b5dba28b7d39f836a94320817a6d51d014e4db16
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-25 17:27:15 +0300

    3 Exercise

---
+++ csharp-basics/exercises/Collections/Exercise 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nameList = new List<string>();
+
+            Console.WriteLine("Enter blank line to stop!");
+
+            Console.WriteLine("Enter the name");
+            string inputName = Console.ReadLine();
+
+            while (!String.IsNullOrEmpty(inputName))
+            {
+                if (!nameList.Contains(inputName))
+                {
+                    nameList.Add(inputName);
+                }else
+                {
+                    Console.WriteLine("Enter next  Name:");
+                    inputName = Console.ReadLine();
+                }
+            }
+            foreach (string Name in nameList)
+            {
+                Console.WriteLine(" " + Name);
+            }
+        }
+    }
+}
+
