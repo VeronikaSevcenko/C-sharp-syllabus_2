2eb548ec0d44564559393bbed00920255ff81809
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-28 23:47:33 +0300

    9 Exercise

---
+++ csharp-basics/exercises/Collections/Exercise 9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vs = new HashSet<string>
+            {
+                "Alex",
+                "Martin",
+                "Gloria",
+                "Melman",
+                "Julien",
+                "Alex"
+            };
+
+            Console.WriteLine("5 string values to HashSet: ");
+
+            foreach (var s in vs)
+            {
+                Console.WriteLine(s);
+            }
+            
+            Console.WriteLine();
+
+            vs.Clear();
+            Console.WriteLine("After removing: ");
+
+            foreach (var s in vs)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Can't to add duplicated values");
+        }
+    }
+}
