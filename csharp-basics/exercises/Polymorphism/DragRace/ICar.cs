0d2d9498141f728f48cb6ee009d58ca1f2817332
VeronikaSevcenko <Vsladkaja95@gmail.com>
2021-12-30 00:29:22 +0300

    Exercise 1 (DragRace)

---
+++ csharp-basics/exercises/Polymorphism/DragRace/ICar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DragRace
+{
+    public interface ICar
+    {
+        void SpeedUp();
+        void SlowDown();
+        string ShowCurrentSpeed();
+        void StartEngine();
+
+    }
+}
