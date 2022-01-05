c18280163a15de0bf48ce9955edbd5b06163682b
VeronikaSevcenko <Vsladkaja95@gmail.com>
2022-01-05 23:02:30 +0300

    Corrected Tik-Tak-Toe

--- csharp-basics/exercises/Arrays/Arrays/TicTacToe/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/TicTacToe/Program.cs
@@ -8,8 +8,6 @@ namespace TicTacToe
 {
     class Program
     {
-        private static string player1 = "";
-        private static string player2 = "";
         private static void Start()
         {
             Console.ForegroundColor = ConsoleColor.White;
@@ -22,158 +20,168 @@ namespace TicTacToe
             Console.WriteLine("  : :  : :'  ..'    : :' .; ; '  ..'    : :' .; :' '_.'  ");
             Console.WriteLine("  :_;  :_;`.__.'    :_;`.__,_;`.__.'    :_;`.__.'`.__.'  ");
             Console.WriteLine("\n");
-
-            Console.WriteLine("player 1 choose 'X' or 'O': Please used capital letter!");
-            player1 = Console.ReadLine();
-
-            Console.WriteLine("player 2 choose 'X' or 'O': Please used capital letter!");
-            player2 = Console.ReadLine();
-
-            if (player1 == "X" && player2 == "O")
-            {
-                Console.WriteLine("player 1 is : X " + "player 2 is : O");
-            }
-            else if (player1 == "O" && player2 == "X")
-            {
-                Console.WriteLine("player 1 is : O " + "player 2 is : X");
-            }
-            else
-            {
-                Console.WriteLine("you chose the same, try again");
-            }
         }
 
         private static char[,] board = new char[3, 3];
         private static void Main(string[] args)
-        { 
+        {
             Start();
             InitBoard();
-            DisplayBoard();
-            
-            while (true)
-            {
-                Console.WriteLine("make a move. Player1");
-            
-                var move = Console.ReadLine();
-                var index1 = int.Parse(move.Split(' ')[0]);
-                var index2 = int.Parse(move.Split(' ')[1]);
-
-                if (board[index1, index2] != 'X' && board[index1, index2] != 'O')
-                {
-                    var player = GetPlayer();
-                    board[index1, index2] = player;
-                    DisplayBoard();
-                }
-
-                var win = GetWinner();
-
-                if (win != 1 && win != -1)
-                    DisplayBoard();
-                
-                if (win == 1)
-                {
-                    Console.WriteLine("You win", player1);
-                }
-                else
-                {
-                    Console.WriteLine("isTie");
-                }
-
-                Console.ReadLine(); 
-            }
-        }
-
-        private static int GetWinner()
-        {
-            if (board[0,0] == board[0,1] && board[0,1] == board[0, 2])
-            {
-                return 1;
-            }
-            else if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2])
-            {
-                return 1;
-            }
-            else if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2])
-            {
-                return 1;
-            }
-            else if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0])
-            {
-                return 1;
-            }
-            else if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2])
-            {
-                return 1;
-            }
-            else if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2])
-            {
-                return 1;
-            }
-            else if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
-            {
-                return 1;
-            }
-            else if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
-            {
-                return 1;
-            }
-            else if (board[0, 0] == 'X' && board[0,1] == 'O' && board[0,2] == 'X' 
-                && board[1,0] == 'O' && board[1,1] == 'X' && board[1,2] == 'O'
-                && board[2,0] == 'X' && board[2,1] == 'O' && board[2,2] == 'X')
-            {
-                return -1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-
-        private static char GetPlayer()
-        {
-            var player1Count = 0;
-            var player2Count = 0;
-            for (int i = 0; i < 2; i++)
-            {
-                for (int j = 0; j < 2; j++)
-                {
-                    if(board[i,j] == player1[0])
-                    {
-                        player1Count++;
-                    }
-                    else if(board[i, j] == player2[0])
-                    {
-                        player2Count++;
-                    }
-                }
-            }
-            if(player1Count == 0 && player2Count == 0)
-            {
-                return player1[0];
-            }
-
-            return player1Count > player2Count ? player2[0] : player1[0];
         }
 
         private static void InitBoard()
         {
-            for (var r = 0; r <= 2; r++)
+            char player = 'O';
+
+            Console.WriteLine("First player " + player);
+
+            for (int r = 0; r < 3; r++)
             {
-                for (var c = 0; c <= 2; c++)
+                for (int c = 0; c < 3; c++)
                     board[r, c] = ' ';
             }
+
+            DisplayBoard(player);
         }
 
-        private static void DisplayBoard()
+        private static void DisplayBoard(char player)
         {
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("  0  " + board[0, 0] + "|" + board[0, 1] + "|" + board[0, 2]);
+
+            Console.WriteLine("  0  " + " 1  " + "2  ");
+            Console.WriteLine("     " + board[0, 0] + "|" + board[0, 1] + "|" + board[0, 2]);
             Console.WriteLine("    --+-+--");
             Console.WriteLine("  1  " + board[1, 0] + "|" + board[1, 1] + "|" + board[1, 2]);
             Console.WriteLine("    --+-+--");
             Console.WriteLine("  2  " + board[2, 0] + "|" + board[2, 1] + "|" + board[2, 2]);
             Console.WriteLine("    --+-+--");
+
+            Game(player);
+        }
+
+        private static void Game(char player)
+        {
+            Console.WriteLine("Hi player, " +  "you need choose space from 0-2 ");
+            int inputRow = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("and once again you need choose space from 0-2 ");
+            int inputColumn = Convert.ToInt32(Console.ReadLine());
+
+            for (int r = inputRow; r <= inputRow; r++)
+            {
+                for (int c = inputColumn; c <= inputColumn; c++)
+
+                    if (board[r, c] == ' ')
+                    {
+                        board[r, c] = player;
+                        player = player == 'X' ? 'O' : 'X';
+                    }
+            }
+
+            char winner = GetWin();
+
+            if (winner != ' ')
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+                Console.WriteLine("  0  " + " 1  " + "2  ");
+                Console.WriteLine("     " + board[0, 0] + "|" + board[0, 1] + "|" + board[0, 2]);
+                Console.WriteLine("    --+-+--");
+                Console.WriteLine("  1  " + board[1, 0] + "|" + board[1, 1] + "|" + board[1, 2]);
+                Console.WriteLine("    --+-+--");
+                Console.WriteLine("  2  " + board[2, 0] + "|" + board[2, 1] + "|" + board[2, 2]);
+                Console.WriteLine("    --+-+--");
+                Console.WriteLine("The winner is" + winner);
+                Restart();
+            }
+            DisplayBoard(player);
+            Draw();
+        }
+
+        private static char GetWin()
+        {
+            if (board[0, 0] == board[0, 1] && board[0, 0] == board[0, 2])
+            {
+                return board[0, 0];
+            }
+            else if (board[1, 0] == board[1, 1] && board[1, 0] == board[1, 2])
+            {
+                return board[1, 0];
+            }
+            else if (board[2, 0] == board[2, 1] && board[2, 0] == board[2, 2])
+            {
+                return board[2, 0];
+            }
+            else if (board[2, 2] == board[1, 1] && board[2, 2] == board[0, 0])
+            {
+                return board[2, 2];
+            }
+            else if (board[0, 2] == board[1, 1] && board[0, 2] == board[2, 0])
+            {
+                return board[0, 2];
+            }
+            else if (board[0, 0] == board[1, 0] && board[0, 0] == board[2, 0])
+            {
+                return board[0, 0];
+            }
+            else if (board[0, 1] == board[1, 1] && board[0, 1] == board[2, 1])
+            {
+                return board[0, 1];
+            }
+            else if (board[0, 2] == board[1, 2] && board[0, 2] == board[2, 2])
+            {
+                return board[0, 2];
+            }
+            return ' ';
+        }
+
+        private static void Draw()
+        {
+            int move = 0;
+
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    if (board[r, c] == 'X' || board[r, c] == 'O')
+                        move++;
+
+                    if (move == 9)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+                        Console.WriteLine("  0  " + " 1  " + "2  ");
+                        Console.WriteLine("     " + board[0, 0] + "|" + board[0, 1] + "|" + board[0, 2]);
+                        Console.WriteLine("    --+-+--");
+                        Console.WriteLine("  1  " + board[1, 0] + "|" + board[1, 1] + "|" + board[1, 2]);
+                        Console.WriteLine("    --+-+--");
+                        Console.WriteLine("  2  " + board[2, 0] + "|" + board[2, 1] + "|" + board[2, 2]);
+                        Console.WriteLine("    --+-+--");
+
+                        Console.WriteLine("The game is over and it's a draw");
+                        Restart();
+                    }
+                }
+            }
+        }
+        private static void Restart()
+        {
+            Console.Write("Wanna play again? yes/no : ");
+            string answer = Console.ReadLine();
+            switch (answer.ToLower())
+            {
+                case "yes":
+                    Console.Clear();
+                    InitBoard();
+                    break;
+                case "no":
+                    System.Environment.Exit(1);
+                    break;
+                default:
+                    Console.WriteLine("Try again");
+                    Restart();
+                    break;
+            }
         }
     }
 }
-
