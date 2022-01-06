5649b862aece109230f82c4a10087f5fe85e1d00
VeronikaSevcenko <Vsladkaja95@gmail.com>
2022-01-06 20:31:18 +0300

    Unit Test number 1

---
+++ csharp-basics/exercises/Arithmetic/Exercise 1/CheckNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_1
+{
+    public class CheckNumbers
+    {
+        public static bool TwoIntegers(int a, int b)
+        {
+            return a == 15 || b == 15 || a + b == 15 || Math.Abs(a - b) == 15;
+        }
+
+    }
+}
