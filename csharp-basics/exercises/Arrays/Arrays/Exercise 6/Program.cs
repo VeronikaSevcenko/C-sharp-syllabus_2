00a770b0fc54cc9a77825a2156b6a4d0e7301b1e
VeronikaSevcenko <Vsladkaja95@gmail.com>
2022-01-08 18:46:23 +0300

    Unit Test Arrays 6

--- csharp-basics/exercises/Arrays/Arrays/Exercise 6/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise 6/Program.cs
@@ -10,27 +10,39 @@ namespace Exercise_6
     {
         static void Main(string[] args)
         {
+
+            int max = 100;
+            int min = 1;
             int[] array1 = new int[10];
-            int[] array2 = new int[array1.Length];
+            int[] array2 = new int[10];
             Random r = new Random();
 
             for (int i = 0; i < array1.Length; i++)
             {
                 array1[i] = r.Next(10);
+                Console.WriteLine("Array 1:" + array1[i]);
             }
             for (int i = 0; i < array2.Length; i++)
             {
                 array2[i] = array1[i];
-            }
-            array1[9] = -7;
-            for (int i = 0; i < array1.Length; i++)
-            {
-                Console.WriteLine("Array 1:" + array1[i]);
-            }
-            for (int i = 0; i < array1.Length; i++)
-            {
                 Console.WriteLine("Array 2: " + array2[i]);
             }
+            LastNumbers.ChangeLastNumber(min, max, array1, array2);
+        }
+    }
+    
+    public class LastNumbers
+    {
+        public static void ChangeLastNumber(int min, int max, int[] array1, int[] array2)
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = r.Next(min, max);
+                array2[i] = array1[i];
+            }
+            array1[array1.Length - 1] = -7;
         }
     }
 }
---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise6.Test/UnitTest1.cs
@@ -0,0 +1,47 @@
+using Exercise_6;
+using NUnit.Framework;
+
+namespace Exercise6.Test
+{
+    public class Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckLastNumberFirstAndSecondArray_ArrayLengthFrom1To100_ResultTrue()
+        {
+            //Arrange
+            int min = 1;
+            int max = 100;
+            int[] array1 = new int[10];
+            int[] array2 = new int[10];
+
+            //Act
+            LastNumbers.ChangeLastNumber( min, max, array1, array2);
+
+            //Assert
+            Assert.AreEqual(array1[array1.Length - 1], -7);
+        }
+
+        [Test]
+        public void CheckIfTwoArraysIsTheSame_ArrayLengthFrom1To100_ResultTrue()
+        {
+            //Arrange
+            int min = 1;
+            int max = 100;
+            int[] array1 = new int[10];
+            int[] array2 = new int[10];
+
+            //Act
+            LastNumbers.ChangeLastNumber(min, max, array1, array2);
+
+            //Assert
+            for (int i = 0; i < array1.Length - 1; i++)
+
+                Assert.AreEqual(array1[i], array2[i]);
+        }
+    }
+}
